a532c8b435d7866ab37991bb44be1e75df31bbef
ghassen <ghassenahmed74@gmail.com>
2023-01-10 20:17:44 +0100

    repair

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 ///////////Variables/////////////
 let output=document.getElementById('out');
-
+let outputLength;
 
 
 
@@ -20,6 +20,13 @@ let output=document.getElementById('out');
 
 function display(value){
     output.value+=value;
+    outputLength=output.value.length;
+    for(let i=0;i<outputLength;i++){
+        for(let j=i+1;j<outputLength;j++){
+            if
+        }
+    }
+    // console.log(outputLength);
 }
 
 function effacer(){
